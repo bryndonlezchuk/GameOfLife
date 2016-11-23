e5318711c3ce3e25bffc8cafa0e732c7d1e89f14
955332826 <955332826@SNH219-14>
2016-11-22 17:21:22 -0800

    void walkthrough

--- P4_GameOfLife/P4_GameOfLife/GameOfLife.c
+++ P4_GameOfLife/P4_GameOfLife/GameOfLife.c
@@ -15,57 +15,11 @@
 #define GRIDHEIGHT 20
 #define GENERATIONS 2
 
-struct Life {
-char name[70];
-int	 gen;		// generation or Z 
-int	 row;		// Y 
-int	 col;		// X 
-int  version;
-int  active;
-};
-
-//------------------------------------------------------------------------------
-//   Function:    main()
-//
-//   Title:       Game of Life
-//
-//   Description: 
-//                Steps through generations in an array.
-//
-//   Programmer:  Lael Smith
-//   
-//   Date:        11/22/2016
-// 
-//   Version:     1.0
-//  
-//   Environment: Intel i5-6600 
-//                Software:  OS: MS Windows 10  
-//                Compiles under Microsoft Visual Studio.Net 2015
-//
-//   Input:		  ...
-//   Output:      ...
-//
-//   Calls:		...
-// 
-//   Returns:	EXIT_SUCCESS (successful execution)
-//				EXIT_FAILURE (unsuccessful execution)
-//
-//   History Log:
-//                10/20/16	LMS	Version 1.0
-// ------------------------------------------------------------------------------
-
 int main(int argc, char *argv[])
 {
-	struct Life life;
-	char lifeGrid[GENERATIONS][GRIDHEIGHT][GRIDWIDTH];
-	int generationCounter = 0;
-	int currentGeneration = 0;
-
-	//variables for files
+	char lifeGrid[GRIDWIDTH][GRIDHEIGHT][GENERATIONS];
 	FILE * inFileHandle = NULL;
 	char filenameInput[FILENAME_MAX] = "";
-
-	//variables for main
 	char prompt = 'n';
 	int quit = 0;
 	int returnValue = EXIT_SUCCESS;
@@ -75,47 +29,32 @@ int main(int argc, char *argv[])
 		switch (prompt)
 		{
 		case 'n':
-		case 'N':		//new file
-			
-			if (argc > 1)
-			{
-				strncpy(filenameInput, argv[1], FILENAME_MAX);
-			}
-			else
-			{
-				puts("Enter the name of the file to read:");
-				fgets(filenameInput, FILENAME_MAX, stdin);
-				if (filenameInput[strlen(filenameInput) - 1] == '\n')
-					filenameInput[strlen(filenameInput) - 1] = '\0';
-				else
-					while (getchar() != '\n')
-						;
-			}
-
-			inFileHandle = fopen(filenameInput, "r");
-			if (inFileHandle == NULL)
-			{
-				printf("Could not open file %s for input.\n"
-					"Press any key to Continue", filenameInput);
-				getch();
-				returnValue = EXIT_FAILURE;
-			}
-			else
-			{
-				//read_data(inFileHandle, &lifeGrid[currentGeneration], currentGeneration, &life);
-				prompt = ' ';
-			}
+		case 'N':
+			//new file
+			//readFile(inFileHandle
 			break;
 		case 'q':
-		case 'Q':		//quit
+		case 'Q':
+			//quit
 			quit = 1;
 			break;
-		default:		//do next generation
-			//calculate
-			//print
-			prompt = getch();
+		default:
+			//next generation
 			break;
 		}
 	}
-	return returnValue;
+}
+
+void walkthrough(char current[GRIDHEIGHT][GRIDWIDTH], char next[GRIDHEIGHT][GRIDWIDTH])
+{
+	int x = GRIDHEIGHT, y = GRIDWIDTH;
+	int i = 0, j = 0;
+	for (i = 0; i < y; i++)
+		for (j = 0; j < x; j++)
+		{
+			if /*isalive*/(current[x][y])
+				next[y][x] = "*";
+			else
+				next[y][x] = " ";
+		}
 }
