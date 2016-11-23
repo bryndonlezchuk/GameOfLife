8a932e84588874291df2c45a8d5ccd32df9b93c6
Lael-Smi <lael.smi@gmail.com>
2016-11-22 16:01:13 -0800

    main() update.

--- P4_GameOfLife/P4_GameOfLife/GameOfLife.c
+++ P4_GameOfLife/P4_GameOfLife/GameOfLife.c
@@ -1,4 +1,46 @@
+//----------------------------------------------------------------------------
+// File: GameOfLife.c
+// Functions:
+//		int main(int argc, char *argv[])
+//----------------------------------------------------------------------------
 
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <math.h>
 #include "ScreenPrinter.c"
+
+#define GRIDWIDTH = 40;
+#define GRIDHEIGHT = 20;
+#define GENERATIONS = 2;
+
+int main(int argc, char *argv[])
+{
+	char lifeGrid[GRIDWIDTH][GRIDHEIGHT][GENERATIONS];
+	FILE * inFileHandle = NULL;
+	char filenameInput[FILENAME_MAX] = "";
+	char prompt = 'n';
+	int quit = 0;
+	int returnValue = EXIT_SUCCESS;
+
+	while (quit = 0)
+	{
+		switch (prompt)
+		{
+		case 'n':
+		case 'N':
+			//new file
+			//readFile(inFileHandle
+			break;
+		case 'q':
+		case 'Q':
+			//quit
+			quit = 1;
+			break;
+		default:
+			//next generation
+			break;
+		}
+	}
+}
