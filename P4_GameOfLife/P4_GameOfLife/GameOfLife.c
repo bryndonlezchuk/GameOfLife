7be076a90958ce93b4af3cee524f93e06bde010b
AnthonyWaddell <a_waddell@hotmail.com>
2016-11-22 16:53:48 -0800

    working int getNeighborsNew()

--- P4_GameOfLife/P4_GameOfLife/GameOfLife.c
+++ P4_GameOfLife/P4_GameOfLife/GameOfLife.c
@@ -11,13 +11,14 @@
 #include <math.h>
 #include "ScreenPrinter.c"
 
-#define GRIDWIDTH = 40;
-#define GRIDHEIGHT = 20;
-#define GENERATIONS = 2;
+#define GRIDWIDTH 40
+#define GRIDHEIGHT 20
+#define GENERATIONS 2
 
 int main(int argc, char *argv[])
 {
-	char lifeGrid[GRIDWIDTH][GRIDHEIGHT][GENERATIONS];
+	char array[GENERATIONS][GRIDHEIGHT][GRIDWIDTH];
+	/*char lifeGrid[GRIDWIDTH][GRIDHEIGHT][GENERATIONS];*/
 	FILE * inFileHandle = NULL;
 	char filenameInput[FILENAME_MAX] = "";
 	char prompt = 'n';
@@ -41,6 +42,76 @@ int main(int argc, char *argv[])
 		default:
 			//next generation
 			break;
+		}
+	}
+}
+
+
+
+#define DEAD 0
+#define ALIVE 1
+
+
+// Checks the value of any given cell. If the cell is dead, it will return false.
+// If the cell is outside of the bounds, it will just return false.
+// If the cell is alive, it will return true. 
+int isAlive(char array1[1][GRIDHEIGHT][GRIDWIDTH], int xcell, int ycell)
+{
+	if (array[xcell][ycell] == DEAD)
+	{
+		return DEAD;
+	}
+	else if (ycell < 0)
+	{
+		return DEAD;
+	}
+	else if (xcell < 0)
+	{
+		return DEAD;
+	}
+	else if (ycell > GRIDHEIGHT)
+	{
+		return DEAD;
+	}
+	else if (xcell > GRIDWIDTH)
+	{
+		return DEAD;
+	}
+	else {
+		return ALIVE;
+	}
+}
+
+// Takes the array of the current generation and the specified cell to count the neighbors. 
+// returns the number of neighbors of the specified cell
+// Will be used in a double for-loop that calculates the next generation to get the neighbor count of each cell
+//int getNeighbors(int array[ROWS][COLUMNS], int xcell, int ycell) {
+//	int neighbors = 0;
+//	neighbors =
+//		isAlive(array, xcell - 1, ycell - 1) +
+//		isAlive(array, xcell, ycell - 1) +
+//		isAlive(array, xcell + 1, ycell - 1) +
+//		isAlive(array, xcell - 1, ycell) +
+//		isAlive(array, xcell, ycell + 1) +
+//		isAlice(array, xcell - 1, ycell + 1) +
+//		isAlive(array, xcell, ycell + 1) +
+//		isAlive(array, xcell + 1, ycell + 1);
+//	return neighbors;
+//}
+
+
+
+int getNeighborsNew(int array[GRIDHEIGHT][GRIDWIDTH])
+{
+	char nextGen[GRIDHEIGHT][GRIDWIDTH] = "";
+	int neighbors = 0;
+
+	for (int i = 1; i < GRIDHEIGHT - 1; i++)
+	{
+		for (int j = 0; j < GRIDWIDTH; j++)
+		{
+			if (array[i - 1][j - 1] == '*')
+				neighbors++;
 		}
 	}
 }
