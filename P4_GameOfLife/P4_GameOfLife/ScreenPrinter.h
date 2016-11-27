e6ff9dfe1c33223dc1a1181e4cb6c2236aa51536
Bryndon Lezchuk <90littlegreenmen@gmail.com>
2016-11-27 11:51:39 -0800

    The start of screenprinter

--- P4_GameOfLife/P4_GameOfLife/ScreenPrinter.h
+++ P4_GameOfLife/P4_GameOfLife/ScreenPrinter.h
@@ -1,8 +1,8 @@
 #ifndef SCREENPRINTER_H
 #define SCREENPRINTER_H
+#include <stdio.h>
 #pragma warning(disable: 4996)
 
 
 
 #endif // !SCREENPRINTER_H
-
--- P4_GameOfLife/ScreenPrinter/Driver.c
+++ P4_GameOfLife/ScreenPrinter/Driver.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>>
+#include <conio.h>
+#pragma warning(disable 4996)
+#define xSIZE 10
+#define ySIZE 10
 
 int main(void)
 {
-	
+	char a1[][xSIZE][ySIZE] = { 0 };
+	printf("Screen printer test, press any key to continue");
+	getche();
+
+	return EXIT_SUCCESS;
 }
