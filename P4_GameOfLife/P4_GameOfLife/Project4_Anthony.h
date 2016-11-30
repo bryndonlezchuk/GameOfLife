76e59a352d058ce19076010513deb3994d0350c0
AnthonyWaddell <a_waddell@hotmail.com>
2016-11-29 16:25:47 -0800

    added .c and .h

---
+++ P4_GameOfLife/P4_GameOfLife/Project4_Anthony.h
@@ -0,0 +1,14 @@
+#ifndef ANTHONY_H
+#define ANTHONY_H
+#define ROWS 20
+#define COLUMNS 40
+#define DEAD 0
+#define ALIVE 1
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning (disable: 4996)
+
+int isAlive(char array[ROWS][COLUMNS], int xcell, int ycell);
+void getNeighbors(char array[ROWS][COLUMNS], int xcell, int ycell);
+
+#endif
