2b55b09e4e2f82079228510f612b73f94dcc7166
955332826 <955332826@SNH219-14>
2016-12-01 15:09:03 -0800

    header

--- P4_GameOfLife/P4_GameOfLife/walkthrough.c
+++ P4_GameOfLife/P4_GameOfLife/walkthrough.c
@@ -1,7 +1,45 @@
+//----------------------------------------------------------------------------
+// File: walkthrough.c
+//
+// Functions:
+//    walkthrough
+//----------------------------------------------------------------------------
 #include "getNeighbors.h"
 #define ALIVE2 '*'
 #define DEAD2 '.'
-
+//----------------------------------------------------------------------------
+// Function: walkthrough
+//
+// Title: Game of Life
+//
+// Description:
+// Scans the entire array of the game of life program, and determines whether
+// or not a cell is dead or alive.
+//
+// Programmer: David C
+//
+// Date: 12/1/16
+//
+// Version: 1.0
+//
+// Environment:
+//	Intel Xeon PC
+//	Software: OS: Windows 10
+//	Compiles under Microsoft Visual Studio 2015
+//
+// Input:
+// None (void)
+//
+// Output:	
+// Passes a value of dead or alive.
+//
+// Parameters: char const *current, char next[ROWS][COLUMNS]
+//
+// Returns: None
+//
+// History Log:
+//   12/1/16 DC completed version 1.0
+//-----------------------------------------------------------------------------
 void walkthrough(char const *current, char next[ROWS][COLUMNS])
 {
 	int x = COLUMNS, y = ROWS;
